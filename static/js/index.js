4dfd2b0812e00d785518bf1f2f10a64237adaf77
jjjabc <jjjabc123@gmail.com>
2015-01-28 18:17:02 +0800

    优化移动端页面显示

--- static/js/index.js
+++ static/js/index.js
@@ -1,66 +1,71 @@
 function winMin() {
 	$("#imgdiv").css("background-image", "url(img/bg_min.jpg)");
-	$("#imgdiv").css("height", $("#imgdiv").width() * "0.4055" + "px");
+	$("#imgdiv").css("height", $("#imgdiv").width() * "0.6043" + "px");
 	$("#Sbriefdiv").show();
 	$("#Stxtdiv").show();
 	$("#contdiv").hide();
-	$("#Stitlediv").css("font-size", $("#imgdiv").height() * "0.3" + "px")
-	$("#Stitlediv").css("padding-left", (($("#imgdiv").width() * 0.05) - 5) + "px")
-	$("#Stitlediv").css("padding-top", ($("#imgdiv").height() *0.2) + "px")
-	$("#Stxtdiv").css("padding-left", (($("#imgdiv").width() * 0.05) - 5) + "px")
-
+		$("#mobile").show();
+	$("#headdiv").hide();
+	$("#Stitlediv").css("font-size", $("#imgdiv").height() * "0.3" + "px");
+	$("#Stitlediv").css("padding-left", (($("#imgdiv").width() * 0.05) - 5) + "px");
+	$("#Stitlediv").css("padding-top", ($("#imgdiv").height() * 0.2) + "px");
+	$("#Stxtdiv").css("padding-left", (($("#imgdiv").width() * 0.05) - 5) + "px");
+	$("#Slogomenudiv").css("margin-left", $("#Stxtdiv").css("padding-left"));
+	$("#Slogomenudiv").css("margin-right", $("#Stxtdiv").css("padding-left"));
 }
 
 function winSmall() {
 	$("#imgdiv").css("background-image", "url(img/bg_s.jpg)");
 	$("#imgdiv").css("height", $("#imgdiv").width() + "px");
-	$("#Sbriefdiv").hide();
-	$("#Stxtdiv").hide();
-	$("#contdiv").show();
+
 	$("#textdiv").css("font-size", $("#imgdiv").width() * "0.03" + "px");
 	$("#titlediv").css("font-size", $("#imgdiv").width() * "0.06" + "px");
 	$("#contdiv").css("margin", "0");
 	$("#contdiv").css("width", "auto");
+	notSmall();
 }
 
 function winMid() {
 	$("#imgdiv").css("background-image", "url(img/bg_m.jpg)");
-	$("#imgdiv").css("height", $("#imgdiv").width() * "0.6043" + "px");
-	$("#Sbriefdiv").hide();
-	$("#Stxtdiv").hide();
-	$("#contdiv").show();
+	$("#imgdiv").css("height", $("#imgdiv").width() * "0.5409" + "px");
+
 	$("#textdiv").css("font-size", $("#imgdiv").width() * "0.02" + "px");
 	$("#titlediv").css("font-size", $("#imgdiv").width() * "0.04" + "px");
 	$("#contdiv").css("margin", "0");
 	$("#contdiv").css("width", "auto");
+	notSmall();
 }
 
 function winMax() {
 	$("#imgdiv").css("background-image", "url(img/bg.jpg)");
 	$("#imgdiv").css("height", $("#imgdiv").width() * "0.4055" + "px");
-	$("#Sbriefdiv").hide();
-	$("#Stxtdiv").hide();
-	$("#contdiv").show();
 	$("#textdiv").css("font-size", $("#imgdiv").width() * "0.02" + "px");
 	$("#titlediv").css("font-size", $("#imgdiv").width() * "0.04" + "px");
 	$("#contdiv").css("margin", "0");
 	$("#contdiv").css("width", "auto");
-
+	notSmall();
 }
 
 function winLag() {
 	$("#imgdiv").css("background-image", "url(img/bg.jpg)");
 	$("#imgdiv").css("height", $("#imgdiv").width() * "0.4055" + "px");
-	$("#Sbriefdiv").hide();
-	$("#Stxtdiv").hide();
-	$("#contdiv").show();
 	$("#textdiv").css("font-size", "32px");
 	$("#contdiv").css("margin", "0 auto");
 	$("#contdiv").css("width", "1280");
 	$("#titlediv").css("font-size", "64px");
-
+	notSmall();
 }
 
 function resizefun() {
 	$("#contdiv").css("padding-top", ($("#imgdiv").height() - $("#briefdiv").height()) / 2 + "px");
+}
+
+function notSmall() {
+	$("#Sbriefdiv").hide();
+	$("#Stxtdiv").hide();
+	$("#mobile").hide();
+	$("#headdiv").show();
+	$("#contdiv").show();
+	$("#headdiv").css("margin-left", $("#briefdiv").css("padding-left"));
+	$("#headdiv").css("margin-right", $("#briefdiv").css("padding-left"));
 }
