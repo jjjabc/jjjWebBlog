a7e603e69f5b8fc12f7cb4dbb08af288200cdccd
jiaxun <jjjabc123@gmail.com>
2015-09-08 01:20:01 +0800

    test dao

--- orm/orm.go
+++ orm/orm.go
@@ -8,7 +8,7 @@ var Red interface {
 	redis.Conn
 }
 
-func Init(ip string, port string,pass string) error {
+func InitPass(ip string, port string,pass string) error {
 	var err error
 	Red, err = redis.Dial("tcp", ip+":"+port,redis.DialPassword(pass))
 	return err
