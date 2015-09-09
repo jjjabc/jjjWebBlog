cc56650cc1a7bb1622c1ca1dfa2cc69264b1c263
jiaxun <jjjabc123@gmail.com>
2015-09-09 15:19:23 +0800

    init admin DB,default password:123456

--- main.go
+++ main.go
@@ -3,14 +3,35 @@ package main
 import (
 	"fmt"
 	"github.com/astaxie/beego"
+	"github.com/garyburd/redigo/redis"
 	"github.com/jjjabc/jjjWebBlog/orm"
 	_ "github.com/jjjabc/jjjWebBlog/routers"
+	"github.com/jjjabc/jjjWebBlog/user"
+	"os"
 )
 
 func main() {
-	if err := orm.Init("10.10.71.37", "50320","RPs0uYV1"); err != nil {
-		fmt.Println("orm init error!")
+	IP := os.Getenv("REDIS_IP")
+	PORT := os.Getenv("REDIS_PORT")
+	PWD := os.Getenv("REDIS_PWD")
+	if IP == "" {
+		IP = "127.0.0.1"
+	}
+	if PORT == "" {
+		PORT = "6379"
+	}
+	if err := orm.Init(IP, PORT, PWD); err != nil {
+		fmt.Println("orm init error!" + err.Error())
 		return
+	}
+	exists, err := redis.Bool(orm.Red.Do("EXISTS", "account:count"))
+	if err != nil {
+		fmt.Println("orm init error!	" + err.Error())
+		return
+	}
+	if !exists {
+		admin := user.JJJuser{Name: "admin", Description: "初始管理员", NickName: "管理员"}
+		err = admin.SigupUser("123456")
 	}
 	beego.SessionOn = true
 	beego.Run()
